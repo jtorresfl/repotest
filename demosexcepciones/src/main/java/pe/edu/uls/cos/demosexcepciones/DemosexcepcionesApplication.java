829cb178b6be1eb39c30e67cb443651b74bc32c8
jtorresfl <jtorresf@ulasalle.edu.pe>
2026-09-17 11:48:51 -0500

    Add files via upload

---
+++ demosexcepciones/src/main/java/pe/edu/uls/cos/demosexcepciones/DemosexcepcionesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uls.cos.demosexcepciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosexcepcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosexcepcionesApplication.class, args);
+	}
+
+}
